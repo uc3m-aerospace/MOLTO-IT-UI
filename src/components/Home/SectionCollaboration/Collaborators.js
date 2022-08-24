e8e757568ac2c19115d4c533ccecb60abbe0ed89
Brandon Escamilla <brandon.escamilla@outlook.com>
2022-08-24 17:22:59 +0200

    Adding static collaborators

--- src/components/Home/SectionCollaboration/Collaborators.js
+++ src/components/Home/SectionCollaboration/Collaborators.js
@@ -8,14 +8,24 @@ import { Spinner } from '@chakra-ui/core';
 const Collaboration = ({ homeApiClient }) => {
   const [collaborators, setCollaborators] = useState([
     {
-      photo: '',
-      name: '',
-      description: ''
+      photo: 'https://drive.google.com/file/d/1KLcRIt4Fs9QRSNQrdY863OQbOnLW6DQR/view?usp=sharing',
+      name: 'Brandon Escamilla',
+      description: 'Space Engineering student, who contribute in Google Summer of Code collaborating in MOLTO. His areas of interest are focused in astrodynamics, space systems, and space propulsion.'
+    },
+    {
+      photo: 'https://drive.google.com/file/d/1LJSqAe_-ov0Sm_sYZOcH5zGW1cygrS8k/view?usp=sharing',
+      name: "Dr. David Morante",
+      description: "Aerospace Engineer, currently doing a PhD on Space Trajectory Optimization, developing new tools that could make interplanetary missions more affordable in terms of travel time or propellant consumed. He is fascinated by the cutting-edge technology developed by the space sector and willing to use this knowledge to help others.  He is also passionate about Astronomy and the mysteries of outer space."
+    },
+    {
+      photo: 'https://i1.rgstatic.net/ii/profile.image/370721810337794-1465398375177_Q128/Manuel-Sanjurjo-Rivo.jpg',
+      name: "Dr. Manuel Sanjurjo",
+      description: "Dr. Manuel Sanjurjo Rivo is an assistant professor at the Aerospace Department of UC3M. His research interests range from celestial mechanics to space tether dynamics or trajectory optimization. He teaches several grade and master courses in flight dynamics and astrodynamics."
     }
   ]);
   const [isLoading, setIsLoading] = useState();
 
-  const getToken = async () => {
+  /*const getToken = async () => {
     try {
       return await getCookie('jwt');
     } catch (error) {
@@ -49,7 +59,7 @@ const Collaboration = ({ homeApiClient }) => {
     };
     fetch();
   }, []);
-
+  */
   return (
     <div className="constrain">
       <p>
